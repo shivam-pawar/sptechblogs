4b52a5321aa3f530b6614da786c3bb15ec0bd1fd
shivam-pawar <shivampawar1038@gmail.com>
2022-12-08 22:03:23 +0530

    added meta tag for GConsole

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Head from "@docusaurus/Head";
 
 import styles from "./index.module.css";
 
@@ -15,6 +16,12 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          <Head>
+            <meta
+              name="google-site-verification"
+              content="GSzYIGKuf_zxvjfMsq4PdOnZvl9vG7gyDCqy8KSIYeE"
+            />
+          </Head>
           <Link className="button button--secondary button--lg" to="/blog">
             Check Blog Posts ➡️
           </Link>
