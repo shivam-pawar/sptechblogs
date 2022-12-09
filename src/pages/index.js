8e0d3adf0d2d52bd349e74e8484396dc6372b89d
shivam-pawar <shivampawar1038@gmail.com>
2022-12-09 19:28:29 +0530

    Updated home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,34 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Head from "@docusaurus/Head";
-
+import HeroImg from "../../static/img/Hero.jpg";
+import Logo from "../../static/img/logonav.png";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Head>
-            <meta
-              name="google-site-verification"
-              content="GSzYIGKuf_zxvjfMsq4PdOnZvl9vG7gyDCqy8KSIYeE"
-            />
-          </Head>
-          <Link className="button button--secondary button--lg" to="/blog">
-            Check Blog Posts ➡️
-          </Link>
-        </div>
+    <div className={styles.myHeroContainer}>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="GSzYIGKuf_zxvjfMsq4PdOnZvl9vG7gyDCqy8KSIYeE"
+        />
+      </Head>
+      <div className={styles.leftContainer}>
+        <h1 className={styles.leftContainer_h1}>
+          <img src={Logo} alt="Logo" />
+          <h2>
+            <i>"Because Code is Poetry"</i>
+          </h2>
+        </h1>
+        <div className={styles.buttonContainer}></div>
       </div>
-    </header>
+      <div className={styles.rightContainer}>
+        <img src={HeroImg} alt="HeroImg" />
+      </div>
+    </div>
   );
 }
 
@@ -39,9 +42,6 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
